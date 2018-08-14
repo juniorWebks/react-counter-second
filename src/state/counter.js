c43a2d3a68255dac00a3e2b40f3e94beeb445f3f
Krzysztof Stefanski <krzysztof.stefanski82@gmail.com>
2018-08-14 10:28:11 +0200

    add state

---
+++ src/state/counter.js
@@ -0,0 +1,12 @@
+const INC = 'counter/INC'
+const DEC = 'counter/DEC'
+
+const incAction = () => ({type:INC})
+const decAction = () => ({type:DEC})
+
+const initialState = {
+    number : 0
+}
+
+
+export default () => {}
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import {createStore, combineReducers} from 'redux'
+import counterReducer from './state/counter'
+
+
+const reducer = combineReducers({                         
+counter : counterReducer
+})
+
+const store = createStore(
+    reducer,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
