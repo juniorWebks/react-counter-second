c56b74d8b907661966ba2f5a2a3e9aaf58e19fbf
Krzysztof Stefanski <krzysztof.stefanski82@gmail.com>
2018-08-14 16:15:36 +0200

    ttd

--- src/state/calculator.js
+++ src/state/calculator.js
@@ -2,6 +2,8 @@ const INPUT = 'calculator/INPUT' //2.typ akcji
 const ADD = 'calculator/ADD'
 const SUBSTRACT = 'calculator/SUBSTRACT'
 
+export const addAction = () => ({ type: ADD })
+
 // 3.action creation dla input
 export const inputAction = number => ({
     type: INPUT,
@@ -11,7 +13,8 @@ export const inputAction = number => ({
 const initialState = { //1. initial state
     result: 0,
     input: 0,
-    isResultShown: false
+    isResultShown: false,
+    isOperation: null
 }
 
 export default (state = initialState, action) => {
@@ -19,8 +22,17 @@ export default (state = initialState, action) => {
         case INPUT:
             return {
                 ...state,
-                input:state.input * 10 + action.number,
+                input: state.input * 10 + action.number,
                 isResultShown: false //czy jest pokazany czy nie ma nie być pokazany
+            }
+        case ADD:
+            return {
+                ...state,
+                result: state.result + state.input,
+                input: 0,
+                isResultShown: true,
+                lastOperation: action.type
+
             }
         default:
             return state
--- src/store.js
+++ src/store.js
@@ -1,14 +1,14 @@
 import { createStore, combineReducers } from 'redux'
-import { addAction } from './state/add'
+// import { addAction } from './state/add'
 import counter, { incAction, decAction } from './state/counter'
 import add from './state/add'
-import calculator,{inputAction} from './state/calculator'
+import calculator, { inputAction, addAction } from './state/calculator'
 
 
 const reducer = combineReducers({
     counter,
-     add,
-     calculator //kolejny reducer po czym robimy kaczke
+    add,
+    calculator //kolejny reducer po czym robimy kaczke
 })
 
 export const store = createStore(
@@ -24,21 +24,37 @@ export const store = createStore(
 
 //automatic testing
 
-const getDiplayedVal = () => {
+const getDisplayedVal = () => {
     const state = store.getState().calculator
-   
-    return state.isResultShown ? state.result : state.input
-   }
-   
-   store.dispatch(inputAction(6))
-   if (getDiplayedVal() !== 6) {
-    throw new Error('Input not works! Should be 6')
-   }
-   store.dispatch(inputAction(7))
-   if (getDiplayedVal() !== 67  ) {
-    throw new Error('Input not works!Should be 67')
-   }
 
+    return state.isResultShown ? state.result : state.input
+}
+
+store.dispatch(inputAction(6))
+if (getDisplayedVal() !== 6) {
+    throw new Error('Input not works! Should be 6!')
+}
+store.dispatch(inputAction(7))
+if (getDisplayedVal() !== 67) {
+    throw new Error('Input not works! Should be 67!')
+}
+store.dispatch(inputAction(3))
+if (getDisplayedVal() !== 673) {
+    throw new Error('Input not works! Should be 673!')
+}
+
+store.dispatch(addAction())
+if (getDisplayedVal() !== 673) {
+    throw new Error('Add not works! Should be 673!')
+}
+store.dispatch(inputAction(3))
+if (getDisplayedVal() !== 3) {
+    throw new Error('Input not works! Should be 3!')
+}
+store.dispatch(addAction())
+if (getDisplayedVal() !== 676) {
+    throw new Error('Add not works! Should be 676!')
+}
 //functions in window are only for manual testing
 
 window.inputAction = number => store.dispatch(inputAction(number))
@@ -47,7 +63,8 @@ window.state = () => {
     const state = store.getState().calculator
 
 
-console.log({...state, display: state.isResultShown ? state.result : state.input
+    console.log({
+        ...state, display: state.isResultShown ? state.result : state.input
 
-})
+    })
 }
